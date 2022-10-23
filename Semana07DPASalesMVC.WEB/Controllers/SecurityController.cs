d2fa77e0f46c40ed908e38e530e14297d651346a
Luis Chang <luis.chang@outlook.com>
2022-10-23 13:55:20 -0500

    Add Login in SecurityController

---
+++ Semana07DPASalesMVC.WEB/Controllers/SecurityController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Semana07DPASalesMVC.WEB.Controllers
+{
+    public class SecurityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View("Login");
+        }
+
+
+    }
+}
